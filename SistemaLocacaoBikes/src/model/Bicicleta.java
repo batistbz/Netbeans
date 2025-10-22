feba300361c32eadd793dd08e7548cd34e0ec956
Beatriz <biabatista1219@gmail.com>
2025-10-22 13:49:55 -0300

    tinha me esquecido do commit

--- SistemaLocacaoBikes/src/model/Bicicleta.java
+++ SistemaLocacaoBikes/src/model/Bicicleta.java
@@ -3,11 +3,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package model;
-
-/**
- *
- * @author biaba
- */
 public class Bicicleta {
+    private int id;
+    private String codigo;
+    private String status;
     
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getCodigo() { return codigo; }
+    public void setCodigo(String codigo) { this.codigo = codigo; }
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
 }
