8c79131ce0101b7138e9617957c57f5c931d49de
Beatriz <biabatista1219@gmail.com>
2025-10-24 08:27:48 -0300

    mudei ne

--- SistemaLocacaoBikes/src/model/Bicicleta.java
+++ SistemaLocacaoBikes/src/model/Bicicleta.java
@@ -14,4 +14,4 @@ public class Bicicleta {
     public void setCodigo(String codigo) { this.codigo = codigo; }
     public String getStatus() { return status; }
     public void setStatus(String status) { this.status = status; }
-}
+}   
